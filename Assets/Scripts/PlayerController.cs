ab105cc31c76c083059a3f4b26f0bc48ba413acb
Emanuel Gomez <emanueljgomez@gmail.com>
2022-04-12 16:38:04 -0300

    Adds enemy (animals) movement and Player proyectile shooting mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,36 @@ public class PlayerController : MonoBehaviour
     // Variables
     public float horizontalInput;
     public float speed = 10.0f;
+    public GameObject projectilePrefab; // Component Variable for Player (assign prefab projectile)
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Keep the player in bounds
-        if (transform.position.x < -10) {
+        if (transform.position.x < -10)
+        {
             transform.position = new Vector3(-10, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > 10) {
+        if (transform.position.x > 10)
+        {
             transform.position = new Vector3(10, transform.position.y, transform.position.z);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a proyectile from the Player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            // Instantiate method: creates a copy of a prefab. Formula: Instantiate(Original object, spawn position, rotation)
+        }
+
+        // Player horizontal movement
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
